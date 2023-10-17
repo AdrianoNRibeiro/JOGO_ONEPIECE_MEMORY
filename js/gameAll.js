debf24c0e3d6b0f054f4f0233f89155140bd3638
adriano.neves <adriano.neves@lojasrede.com.br>
2023-10-17 08:10:11 -0300

    ajuste responsivo

--- js/gameAll.js
+++ js/gameAll.js
@@ -3,6 +3,12 @@ const btn_gamePoke = document.querySelector('#btn_gamePoke');
 const btn_gameDragon = document.querySelector('btn_gameDragon');
 const btn_gameDemon = document.querySelector('#btn_gameDemon');
 const btn_records = document.querySelector('#btn_records');
+const menuHamburger = document.querySelector('#menuHamburger');
+const containerMenu = document.querySelector('.container-menu');
+
+menuHamburger.addEventListener('click',(evt)=>{
+    containerMenu.classList.toggle('displayFlex');
+})
 
 btn_gamePiece.addEventListener('click',(evt)=>{
     window.location.href = '../pages/gameOne.html';
@@ -19,6 +25,8 @@ btn_gameDragon.addEventListener('click',(evt)=>{
 btn_gameDemon.addEventListener('click',(evt)=>{
     window.location.href = '../pages/gameDemon.html';
 })
+
+
 
 
 
