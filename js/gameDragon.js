3c2e947c4ca476e8231569b370c4b4a98756ce5c
adriano.neves <adriano.neves@lojasrede.com.br>
2023-10-19 11:03:13 -0300

    m3

--- js/gameDragon.js
+++ js/gameDragon.js
@@ -6,18 +6,18 @@ const characterDisplay = document.querySelector('#characterDisplay');
 const characterName = document.querySelector('#characterName');
 
 const characters = [
-    'Brook',
-    'Chopper',
-    'Franky',
-    'Jinbe',
-    'Luffy',
-    'Nami',
-    'Nico_Robin',
-    'Sanji',
-    'Usopp',
-    'Zoro',
-    'Garp',
-    'Hawk',
+    'Bulma',
+    'Cell',
+    'Chichi',
+    'Freeza',
+    'Gohan',
+    'Goku',
+    'Goten',
+    'Kuririn',
+    'MajinBoo',
+    'Piccolo',
+    'Trunks',
+    'Vegeta',
 ];
 
 const createElement=(tag,className)=>{
@@ -46,7 +46,7 @@ const checkCards=()=>{
         firstCard.firstChild.classList.add('disable-card');
         secondCard.firstChild.classList.add('disable-card');
 
-        characterDisplay.setAttribute('src',`../img/imgOne/${firstCharacter}.jpg`);
+        characterDisplay.setAttribute('src',`../img/imgDragon/${firstCharacter}.jpg`);
         characterName.innerHTML = firstCharacter;
 
         firstCard = "";
@@ -85,7 +85,7 @@ const createCard=(character)=>{
     const front = createElement('div', 'face front');
     const back = createElement('div', 'face back');
 
-    front.style.backgroundImage = `url('../img/imgOne/${character}.jpg')`;
+    front.style.backgroundImage = `url('../img/imgDragon/${character}.jpg')`;
 
     card.appendChild(front);
     card.appendChild(back);
